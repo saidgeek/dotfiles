952b527d4c7a46979bbbdd1c10c10cecff322c3f
saidgeek <me@saidgeek.land>
2021-07-22 01:31:15 -0400

    chore: add config to nvim-tree

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -8,23 +8,41 @@ set('auto_open', 1)
 set('auto_close', 1)
 set('quit_on_open', 1)
 set('update_cwd', 0)
+set('group_empty', 1)
+set('lsp_diagnostics', 1)
+set('highlight_opened_files', 1)
+set('follow', 0)
 set('auto_ignore_ft', {'dashboard'})
 set('icon_padding', ' ')
 set('show_icons', {
   git = 1,
   folders = 1,
   files = 1,
+  folder_arrows = 1,
 })
 
 set('icons', {
   default = "﬒",
   symlink = "",
   git = {
-    unstaged = "",
+    unstaged = "",
     staged = "ﮒ",
     unmerged = "",
     renamed = "",
     untracked = ""
   },
-  folder = {default = "", open = ""}
+  folder = {
+    arrow_open = "",
+    arrow_closed = "",
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+  },
+  lsp = {
+    hint = "",
+    info = "",
+    warning = "",
+    error = "",
+  }
 })
