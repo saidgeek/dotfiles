b239d41317a3114c2f1488fec9239bd309fe998a
saidgeek <me@saidgeek.land>
2021-07-22 00:09:25 -0400

    chore: modify opacity alacritty term

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -2,6 +2,7 @@ require('bufferline').setup {
   options = {
     show_buffer_close_icon = false,
     show_close_icon = false,
+    diagnostics = "nvim_lsp",
     offsets = {
       {
         filetype = 'NvimTree',
