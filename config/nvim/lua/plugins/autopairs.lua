32e85f4c2e704debb87756e17218573c57b2d20a
saidgeek <me@saidgeek.land>
2021-07-22 01:31:46 -0400

    chore: add config to autopairs

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -1,5 +1,11 @@
 require('nvim-autopairs').setup({
   fast_wrap = {
     map = '<C-e>',
-  },
+    chars = { '{', '[', '(', '"', "'" },
+    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
+    end_key = '$',
+    keys = 'qwertyuiopzxcvbnmasdfghjkl',
+    check_comma = true,
+    hightlight = 'Search'
+  }
 })
