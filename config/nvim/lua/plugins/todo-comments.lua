d0ff432e350634ca9679a9aa128dfb395614f6c9
saidgeek <me@saidgeek.land>
2021-07-22 02:09:32 -0400

    chore: remove config search in todo-comments

--- config/nvim/lua/plugins/todo-comments.lua
+++ config/nvim/lua/plugins/todo-comments.lua
@@ -38,19 +38,5 @@ require('todo-comments').setup {
     hint = { "LspDiagnosticsDefaultHint", "#10B981" },
     default = { "Identifier", "#7C3AED" },
   },
-  search = {
-    command = "rg",
-    args = {
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-    },
-    -- regex that will be used to match keywords.
-    -- don't replace the (KEYWORDS) placeholder
-    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-    -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
-  },
 }
 
