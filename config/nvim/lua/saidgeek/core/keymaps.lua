712c5f21cb9570ba02b7a80fb6c82691de6f2a98
Andrés Espinace <andresespinace.globant@latam.com>
2022-11-04 13:01:57 -0300

    feat: add essentials and telescope

--- config/nvim/lua/saidgeek/core/keymaps.lua
+++ config/nvim/lua/saidgeek/core/keymaps.lua
@@ -2,6 +2,35 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+-- use kjkj to exit insert mode
+keymap.set("i", "kjkj", "<ESC>")
+
+-- maximize current window
+keymap.set("n", "<leader>m", ":MaximizerToggle<CR>")
+
+-- window manager
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
 -- file explorer
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+
+-- telescope git commands
+keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>")
+keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>")
+keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>")
+keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>")
--- config/nvim/lua/saidgeek/plugins-setup.lua
+++ config/nvim/lua/saidgeek/plugins-setup.lua
@@ -3,7 +3,6 @@ local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
-  print(install_path)
   if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
     vim.cmd([[packadd packer.nvim]])
@@ -33,6 +32,12 @@ end
 return packer.startup(function(use)
   use("wbthomason/packer.nvim")
   use("nvim-lua/plenary.nvim")
+  use("christoomey/vim-tmux-navigator") -- add shortcuts for jump into splits
+  use("szw/vim-maximizer") -- maximize current window
+
+  -- essential plugins
+	use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
+  use("numToStr/Comment.nvim")
 
   -- colorschema
   use ({ 'projekt0n/github-nvim-theme' })
@@ -45,6 +50,10 @@ return packer.startup(function(use)
 
   -- statusline
 	use("nvim-lualine/lualine.nvim")
+
+  -- fuzzy finding w/ telescope
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
   if packer_bootstrap then
     require("packer").sync()
