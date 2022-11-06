fe934a8c2863e4efd11b0381f809e1e068d8feef
saidgeek <me@saidgeek.land>
2022-11-06 02:42:52 -0300

    feat: add neogit

--- config/nvim/lua/saidgeek/plugins-setup.lua
+++ config/nvim/lua/saidgeek/plugins-setup.lua
@@ -1,16 +1,16 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
 
-    return true
-  end
+		return true
+	end
 
-  return false
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -26,46 +26,47 @@ vim.cmd([[
 -- import packer safely
 local status, packer = pcall(require, "packer")
 if not status then
-  return
+	return
 end
 
 return packer.startup(function(use)
-  use("wbthomason/packer.nvim")
-  use("nvim-lua/plenary.nvim")
-  use("christoomey/vim-tmux-navigator") -- add shortcuts for jump into splits
-  use("szw/vim-maximizer") -- maximize current window
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("christoomey/vim-tmux-navigator") -- add shortcuts for jump into splits
+	use("szw/vim-maximizer") -- maximize current window
 
-  -- essential plugins
+	-- essential plugins
 	use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
-  use("numToStr/Comment.nvim")
+	use("numToStr/Comment.nvim")
+	use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
 
-  -- colorschema
-  use ({ 'projekt0n/github-nvim-theme' })
+	-- colorschema
+	use({ "projekt0n/github-nvim-theme" })
 
-  -- file explorer
-  use("nvim-tree/nvim-tree.lua")
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
 
-  -- icons
-  use("kyazdani42/nvim-web-devicons")
+	-- icons
+	use("kyazdani42/nvim-web-devicons")
 
-  -- statusline
+	-- statusline
 	use("nvim-lualine/lualine.nvim")
 
-  -- fuzzy finding w/ telescope
+	-- fuzzy finding w/ telescope
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
 	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
-  -- autocompletion
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-buffer")
+	-- autocompletion
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
 	use("hrsh7th/cmp-path")
 
-  -- snippets
+	-- snippets
 	use("L3MON4D3/LuaSnip") -- snippet engine
 	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
 	use("rafamadriz/friendly-snippets") -- useful snippets
 
-  -- managing & installing lsp servers, linters & formatters
+	-- managing & installing lsp servers, linters & formatters
 	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
 	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
 
@@ -76,7 +77,7 @@ return packer.startup(function(use)
 	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
 	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
 
-  -- formatting & linting
+	-- formatting & linting
 	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
 	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
 
@@ -95,8 +96,7 @@ return packer.startup(function(use)
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
-  if packer_bootstrap then
-    require("packer").sync()
-  end
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
-
