05bd3f95e14c7f196445de5028156564134da027
saidgeek <me@saidgeek.land>
2022-11-08 01:46:38 -0300

    feat: add markdownlit to null-ls

--- config/nvim/lua/saidgeek/plugins/lsp/null-ls.lua
+++ config/nvim/lua/saidgeek/plugins/lsp/null-ls.lua
@@ -25,12 +25,14 @@ null_ls.setup({
 		formatting.prettier, -- js/ts formatter
 		formatting.stylua, -- lua formatter
 		formatting.jq,
+		formatting.markdownlint,
 		diagnostics.eslint_d.with({ -- js/ts linter
 			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
 			condition = function(utils)
 				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
 			end,
 		}),
+		diagnostics.markdownlint,
 	},
 	-- configure format on save
 	on_attach = function(current_client, bufnr)
