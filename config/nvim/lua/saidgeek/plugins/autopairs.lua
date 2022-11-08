abe9e2fdbc1608d8927990bef1a77946b3623789
saidgeek <me@saidgeek.land>
2022-11-08 01:37:53 -0300

    fix: correct autopairs config and null-ls for nvim 0.8.0

--- config/nvim/lua/saidgeek/plugins/autopairs.lua
+++ config/nvim/lua/saidgeek/plugins/autopairs.lua
@@ -1,37 +1,25 @@
--- import nvim-treesitter plugin safely
-local status, treesitter = pcall(require, "nvim-treesitter.configs")
+local status, autopairs = pcall(require, "nvim-autopairs")
 if not status then
   return
 end
 
--- configure treesitter
-treesitter.setup({
-  -- enable syntax highlighting
-  highlight = {
-    enable = true,
+autopairs.setup({
+  check_ts = true,
+  ts_config = {
+    lua = { "string" },
+    javascript = { "template_string" },
   },
-  -- enable indentation
-  indent = { enable = true },
-  -- enable autotagging (w/ nvim-ts-autotag plugin)
-  autotag = { enable = true },
-  -- ensure these language parsers are installed
-  ensure_installed = {
-    "json",
-    "javascript",
-    "typescript",
-    "tsx",
-    "yaml",
-    "html",
-    "css",
-    "markdown",
-    "svelte",
-    "graphql",
-    "bash",
-    "lua",
-    "vim",
-    "dockerfile",
-    "gitignore",
-  },
-  -- auto install above language parsers
-  auto_install = true,
 })
+
+
+local cmp_autopairs_status, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+if not cmp_autopairs_status then
+  return
+end
+
+local cmp_status, cmp = pcall(require, "cmp")
+if not cmp_status then
+  return
+end
+
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
--- config/nvim/lua/saidgeek/plugins/lsp/null-ls.lua
+++ config/nvim/lua/saidgeek/plugins/lsp/null-ls.lua
@@ -1,6 +1,12 @@
+local utils = require("saidgeek.core.utils")
+if not utils.expect_nvim_version() then
+	error("null-ls config is not support nvm version smaller than 0.8.0")
+	return
+end
+
 local setup, null_ls = pcall(require, "null-ls")
 if not setup then
-  return
+	return
 end
 
 -- for conciseness
@@ -12,38 +18,37 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- configure null_ls
 null_ls.setup({
-  -- setup formatters & linters
-  sources = {
-    --  to disable file types use
-    --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-    formatting.prettier, -- js/ts formatter
-    formatting.stylua, -- lua formatter
-    formatting.jq,
-    formatting.alex,
-    diagnostics.eslint_d.with({ -- js/ts linter
-      -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-      condition = function(utils)
-        return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-      end,
-    }),
-  },
-  -- configure format on save
-  on_attach = function(current_client, bufnr)
-    if current_client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.formatting({
-            filter = function(client)
-              --  only use null-ls for formatting instead of lsp server
-              return client.name == "null-ls"
-            end,
-            bufnr = bufnr,
-          })
-        end,
-      })
-    end
-  end,
+	-- setup formatters & linters
+	sources = {
+		--  to disable file types use
+		--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
+		formatting.prettier, -- js/ts formatter
+		formatting.stylua, -- lua formatter
+		formatting.jq,
+		diagnostics.eslint_d.with({ -- js/ts linter
+			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
+			condition = function(utils)
+				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
+			end,
+		}),
+	},
+	-- configure format on save
+	on_attach = function(current_client, bufnr)
+		if current_client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({
+						filter = function(client)
+							--  only use null-ls for formatting instead of lsp server
+							return client.name == "null-ls"
+						end,
+						bufnr = bufnr,
+					})
+				end,
+			})
+		end
+	end,
 })
