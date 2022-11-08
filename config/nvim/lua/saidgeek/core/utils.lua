74acd9927b174f735292cc1488ea49a32f3f6b53
saidgeek <me@saidgeek.land>
2022-11-08 01:37:53 -0300

    fix: correct autopairs config and null-ls for nvim 0.8.0

---
+++ config/nvim/lua/saidgeek/core/utils.lua
@@ -0,0 +1,21 @@
+local utils = {}
+
+-- expect_nvim_version
+local nvim_info = vim.version()
+local nvim_version = nvim_info.major .. "." .. nvim_info.minor .. "." .. nvim_info.patch
+
+function utils.expect_nvim_version(version)
+	local expect_version = version
+	if not (type(expect_version) == "string") then
+		expect_version = "0.8.0"
+	end
+
+	print(expect_version)
+
+	if nvim_version ~= expect_version then
+		return false
+	end
+	return true
+end
+
+return utils
