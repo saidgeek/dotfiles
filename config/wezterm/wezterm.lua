2d1099f6d37e259b0da0dfb68ba7af15c04214d1
saidgeek <me@saidgeek.land>
2023-04-03 22:36:16 -0400

    feat: add wezterm config file

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.font = wezterm.font("Victor Mono Nerd Font", { weight = "Bold" })
+config.font_size = 10.0
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.window_padding = {
+	left = 5,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.window_decorations = "NONE"
+config.front_end = "WebGpu"
+
+config.color_scheme = "Catppuccin Macchiato"
+config.default_prog = { "/usr/bin/tmux", "new-session", "-A", "-s", "Main" }
+
+return config
