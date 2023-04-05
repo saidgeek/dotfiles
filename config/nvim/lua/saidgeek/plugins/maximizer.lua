df13f1f7574743e973fee3f280e595dcadeb7d1a
saidgeek <me@saidgeek.land>
2023-04-04 22:48:47 -0400

    feat(config): change config to nvim and tmux

--- config/nvim/lua/saidgeek/plugins/maximizer.lua
+++ config/nvim/lua/saidgeek/plugins/maximizer.lua
@@ -2,8 +2,6 @@ return {
 	"szw/vim-maximizer",
 	keys = {
 		{ "<leader>m", ":MaximizerToggle<CR>", { "n" } },
-		{ "<leader>m", ":MaximizerToggle<CR>", { "v" } },
-		{ "<C-m>", ":MaximizerToggle<CR>", { "i" } },
 	},
 	cmd = "MaximizerToggle",
 }
