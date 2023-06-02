046b3b09b70a7e9360b0f211cf94cf21749b1566
saidgeek <me@saidgeek.land>
2023-06-01 23:02:22 -0400

    feat(emacs): add custom-val file

---
+++ config/emacs/custom-val.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" default))
+ '(package-selected-packages
+   '(magit ivy-rich counsel swiper which-key evil-collection evil general rainbow-delimiters doom-modeline all-the-icons doom-themes use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
