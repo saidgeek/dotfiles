f57e10b5a08b6dd2ec2af96f7687b79dab9ed739
saidgeek <me@saidgeek.land>
2023-06-17 17:06:28 -0400

    feat(emacs): impl config from readme org file

--- config/emacs/custom-val.el
+++ config/emacs/custom-val.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" default))
  '(package-selected-packages
-   '(eglot rust-mode typescript-mode company-box company lsp-treemacs lsp-ivy lsp-ui lsp-mode counsel-projectile projectile magit ivy-rich counsel swiper which-key evil-collection evil general rainbow-delimiters doom-modeline all-the-icons doom-themes use-package)))
+   '(org-superstar org-tempo org-bullets flycheck tsx-mode rjsx-mode web-mode nix-mode vterm-mode vterm eglot rust-mode typescript-mode company-box company lsp-treemacs lsp-ivy lsp-ui lsp-mode counsel-projectile projectile magit ivy-rich counsel swiper which-key evil-collection evil general rainbow-delimiters doom-modeline all-the-icons doom-themes use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
