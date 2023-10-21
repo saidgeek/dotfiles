5d67937cc80d17fd363f91956a8a53c899c4a70a
Andrés Espinace <andresespinace.globant@latam.com>
2023-10-20 22:26:15 -0300

    chore: refact configs

--- config/nvim/lua/saidgeek/plugins/lsp.lua
+++ config/nvim/lua/saidgeek/plugins/lsp.lua
@@ -56,6 +56,7 @@ return {
 		{ "jose-elias-alvarez/typescript.nvim" },
 		{ "hrsh7th/cmp-nvim-lsp" },
 		{ "williamboman/mason.nvim" },
+		{ "simrat39/rust-tools.nvim" },
 	},
 	config = function()
 		local lspconfig = require("lspconfig")
@@ -132,9 +133,37 @@ return {
 
 		setup_servers(lspconfig, on_attach, capabilities)
 
-		require("saidgeek.plugins.lsp.langs.rust-analyzer").setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
+		require("rust-tools").setup({
+			tools = {
+				runnables = {
+					use_telescope = true,
+				},
+				inlay_hints = {
+					auto = true,
+					show_parameter_hints = false,
+					parameter_hints_prefix = "",
+					other_hints_prefix = "",
+				},
+			},
+
+			-- all the opts to send to nvim-lspconfig
+			-- these override the defaults set by rust-tools.nvim
+			-- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+			server = {
+				-- on_attach is a callback called when the language server attachs to the buffer
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					-- to enable rust-analyzer settings visit:
+					-- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+					["rust-analyzer"] = {
+						-- enable clippy on save
+						checkOnSave = {
+							command = "clippy",
+						},
+					},
+				},
+			},
 		})
 
 		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -6,9 +6,8 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-
-config.font = wezterm.font("VictorMono Nerd Font", { weight = "Semibold" })
-config.font_size = 10.0
+config.font = wezterm.font("VictorMono Nerd Font", { weight = "Bold" })
+config.font_size = 11.0
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.window_padding = {
@@ -19,26 +18,25 @@ config.window_padding = {
 }
 
 config.window_frame = {
-  border_left_width = '0.2cell',
-  border_right_width = '0.2cell',
-  border_bottom_height = '0.2cell',
-  border_top_height = '0.2cell',
-  border_left_color = '#232323',
-  border_right_color = '#232323',
-  border_bottom_color = '#232323',
-  border_top_color = '#232323',
+	border_left_width = "0.2cell",
+	border_right_width = "0.2cell",
+	border_bottom_height = "0.2cell",
+	border_top_height = "0.2cell",
+	border_left_color = "#232323",
+	border_right_color = "#232323",
+	border_bottom_color = "#232323",
+	border_top_color = "#232323",
 }
-
 
 config.initial_cols = 150
 config.initial_rows = 35
 -- config.window_decorations = "NONE"
--- config.front_end = "OpenGL"
+config.front_end = "OpenGL"
 -- config.front_end = "WebGpu"
-config.enable_wayland = true
+-- config.enable_wayland = true
 
 config.color_scheme = "Catppuccin Macchiato"
-config.default_prog = { "tmux", "new-session", "-A", "-s", "Main" }
+-- config.default_prog = { "tmux", "new-session", "-A", "-s", "Main" }
 
 config.warn_about_missing_glyphs = true
 
